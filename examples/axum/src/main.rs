2dd78160a4f44386e7a618f4699d910f7d372d50
Mattia Penati <mattia.penati@protonmail.com>
2026-01-29 22:52:07 +0100

    Fix example in readme

--- examples/axum/src/main.rs
+++ examples/axum/src/main.rs
@@ -1,12 +1,13 @@
 use axum::Router;
+use tower_embed::{Embed, ServeEmbed};
 
-#[derive(tower_embed::Embed)]
+#[derive(Embed)]
 #[embed(folder = "assets")]
 struct Assets;
 
 #[tokio::main]
 async fn main() {
-    let assets = tower_embed::ServeEmbed::<Assets>::new();
+    let assets = ServeEmbed::<Assets>::new();
     let router = Router::new().fallback_service(assets);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
