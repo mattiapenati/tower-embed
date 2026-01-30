d12323be81a8d42a989138c52986d32badfe5f34
Mattia Penati <mattia.penati@protonmail.com>
2026-01-31 00:22:14 +0100

    Example for custom 404 page

---
+++ examples/custom-404/src/main.rs
@@ -0,0 +1,19 @@
+use axum::Router;
+use tower_embed::{Embed, EmbedExt, ServeEmbedBuilder};
+
+#[derive(Embed)]
+#[embed(folder = "assets")]
+struct Assets;
+
+#[tokio::main]
+async fn main() {
+    let assets = ServeEmbedBuilder::new()
+        .not_found_service(Assets::not_found_page("404.html"))
+        .build::<Assets>();
+    let router = Router::new().fallback_service(assets);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    axum::serve::serve(listener, router).await.unwrap();
+}
